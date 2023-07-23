c521dd62c1b3fd0de0344f12b67ca6bf1a3b74b7
jangjunha <hi@jangjunha.me>
2023-07-23 20:50:42 +0900

    publish to gemfury, not github packages

--- jvm/build.gradle.kts
+++ jvm/build.gradle.kts
@@ -9,16 +9,30 @@ subprojects {
     configure<PublishingExtension> {
         repositories {
             maven {
-                name = "GithubPackages"
-                url = uri("https://maven.pkg.github.com/jangjunha/ftgo-proto")
+                name = "fury"
+                url = uri("https://maven.fury.io/jangjunha/")
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
                 credentials {
-                    username = project.findProperty("gpr.user") as? String ?: System.getenv("USERNAME")
-                    password = project.findProperty("gpr.key") as? String ?: System.getenv("TOKEN")
+                    username = System.getenv("FURY_TOKEN")
+                    password = "NOPASS"
                 }
             }
         }
         publications {
-            register<MavenPublication>("gpr") {
+            register<MavenPublication>("fury") {
+                pom {
+                    name.set(project.name)
+                    url.set("https://github.com/jangjunha/ftgo-proto")
+                    developers {
+                        developer {
+                            id.set("jangjunha")
+                            name.set("jangjunha")
+                        }
+                    }
+                }
+
                 from(components["java"])
             }
         }
