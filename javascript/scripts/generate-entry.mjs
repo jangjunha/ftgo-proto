8d4ed222a96dc69551699f97fa2c542559095f8b
jangjunha <hi@jangjunha.me>
2023-08-03 18:01:11 +0900

    fix: javascript lib compile error

--- javascript/scripts/generate-entry.mjs
+++ javascript/scripts/generate-entry.mjs
@@ -14,6 +14,6 @@ fs.writeFile(
     "lib/index.d.ts",
     dirs
         .filter(f => f.endsWith(".d.ts"))
-        .map(f => `export * from "./${f}";`)
+        .map(f => `export type * from "./${f}";`)
         .join("\n")
 );
